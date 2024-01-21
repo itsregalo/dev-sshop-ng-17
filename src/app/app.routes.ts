42f630e8e5ab8f5b33ab6179b88c03826eda82f0
itsregalo <itsregalo047@gmail.com>
2024-01-21 23:51:11 +0300

    fix ng modules and added primereact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { InventoryComponent } from './pages/inventory/inventory.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'inventory',
+    component: InventoryComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
